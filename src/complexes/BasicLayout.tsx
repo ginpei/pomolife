a2bc41b61c6f2012c6cbd54d747130428a2075dd
ginpei <ginpei@gmail.com>
2020-03-07 00:48:42 -0800

    set up basic page layout

---
+++ src/complexes/BasicLayout.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './BasicLayout.css';
+
+type BasicLayoutProps = React.ComponentPropsWithRef<'div'>;
+
+const BasicLayout: React.FC<BasicLayoutProps> = (props) => {
+  const { children, ...restProps } = props;
+
+  return (
+    <div className="BasicLayout" {...restProps}>
+      <BasicPageHeader />
+      <BasicPageBody>{children}</BasicPageBody>
+      <BasicPageFooter />
+    </div>
+  );
+};
+
+const BasicPageHeader = () => {
+  return (
+    <div className="BasicPageHeader">
+      <div className="ui-container">
+        <a href="/">Header</a>
+      </div>
+    </div>
+  );
+};
+
+const BasicPageBody: React.FC = (props) => {
+  return (
+    <div className="BasicPageBody ui-container">
+      {props.children}
+    </div>
+  );
+};
+
+const BasicPageFooter = () => {
+  return (
+    <div className="BasicPageFooter">
+      <div className="ui-container">
+        Footer
+      </div>
+    </div>
+  );
+};
+
+export default BasicLayout;
---
+++ src/screens/TimerPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import BasicLayout from '../complexes/BasicLayout';
+
+const TimerPage: React.FC = () => {
+  return (
+    <BasicLayout className="TimerPage">
+      <h1>Timer Page</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae similique quod, sed reiciendis laudantium obcaecati veritatis id a neque necessitatibus quia, natus magnam laborum asperiores ipsum ipsam, maiores nisi quasi.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam est officia a ipsa debitis voluptatum quo eligendi doloremque accusamus labore fugiat quas recusandae iure, voluptatem reiciendis similique eaque, tenetur porro.</p>
+    </BasicLayout>
+  );
+};
+
+export default TimerPage;
