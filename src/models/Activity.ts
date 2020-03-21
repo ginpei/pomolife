8075530333f62ba70d52e9fc0d4360a81359e6a1
ginpei <ginpei@gmail.com>
2020-03-21 12:11:01 -0700

    extract dummy data

--- src/models/Activity.ts
+++ src/models/Activity.ts
@@ -42,3 +42,39 @@ export function getEmoji(feeling: ActivityFeeling) {
 export function setDone(activity: Activity, now = Date.now()) {
   activity.doneAt = now;
 }
+
+
+const dummyActivitySource: Activity[] = [
+  {
+    doneAt: new Date('2020/04/14 12:00:00.0000Z').getTime(),
+    elapse: 25 * 1000 * 60,
+    feeling: 'great',
+    title: 'hoge',
+  },
+  {
+    doneAt: new Date('2020/04/14 12:25:00.0000Z').getTime(),
+    elapse: 25 * 1000 * 60,
+    feeling: 'good',
+    title: 'fuga',
+  },
+  {
+    doneAt: new Date('2020/04/14 12:50:00.0000Z').getTime(),
+    elapse: 25 * 1000 * 60,
+    feeling: 'bad',
+    title: 'ugh',
+  },
+];
+
+export const dummyActivities: Activity[] = Array.from(
+  { length: 30 },
+  (_, i) => {
+    const h24 = 24 * 1000 * 60 * 60;
+    const sourceIndex = Math.floor(Math.random() * dummyActivitySource.length);
+    const source = dummyActivitySource[sourceIndex];
+    const item: Activity = {
+      ...source,
+      doneAt: source.doneAt + Math.floor(Math.random() * h24),
+    };
+    return item;
+  },
+).sort((a, b) => a.doneAt - b.doneAt);
