a4bb8624197a93a2f9ef6b9415852a479a8f3c04
ginpei <ginpei@gmail.com>
2020-03-25 23:46:30 -0700

    update to select felings when sprint done

--- src/screens/TimerPage.tsx
+++ src/screens/TimerPage.tsx
@@ -7,7 +7,39 @@ import { TimerForm } from '../simples/TimerForm';
 import './TimerPage.scss';
 
 export const TimerPage: React.FC = () => {
-  const [activityLog] = useState(Activity.dummyActivities);
+  const url = '/D0002070098_00000_A_001.m4a';
+  const [bell] = useState(new Audio(url));
+  const [activityLog, setActivityLog] = useState<Activity.Activity[]>([]);
+  const [popupVisible, setPopupVisible] = useState(false);
+
+  useBeyondSprintEffect((lastPeriod) => {
+    bell.currentTime = 0;
+    bell.play();
+
+    activityLog.push({
+      doneAt: lastPeriod,
+      elapse: 0,
+      feeling: '',
+      title: '',
+    });
+    setActivityLog(activityLog);
+
+    setPopupVisible(true);
+  });
+
+  const onLastFeelingSelect = (feeling: Activity.ActivityFeeling) => {
+    bell.pause();
+
+    const lastActivity = activityLog[activityLog.length - 1];
+    if (!lastActivity) {
+      return;
+    }
+
+    lastActivity.feeling = feeling;
+    setActivityLog(activityLog);
+
+    setPopupVisible(false);
+  };
 
   return (
     <div className="TimerPage">
@@ -29,34 +61,66 @@ export const TimerPage: React.FC = () => {
           <TimerForm />
         </div>
       </footer>
-      <ChimePopup />
+      <FeelingPopup
+        onSelect={onLastFeelingSelect}
+        visible={popupVisible}
+      />
     </div>
   );
 };
 
-const ChimePopup: React.FC = () => {
-  const url = '/D0002070098_00000_A_001.m4a';
-  const [bell] = useState(new Audio(url));
-  const [visible, setVisible] = useState(false);
-
-  useBeyondSprintEffect(() => {
-    setVisible(true);
-    bell.currentTime = 0;
-    bell.play();
-  });
-
-  const onStopClick = () => {
-    setVisible(false);
-    bell.pause();
-  };
+const FeelingPopup: React.FC<{
+  onSelect: (feeling: Activity.ActivityFeeling) => void;
+  visible: boolean;
+}> = ({ onSelect, visible }) => {
+  const feelings: Activity.ActivityFeeling[] = ['great', 'good', 'bad'];
 
   return (
-    <div className="TimerPage-ChimePopup" data-visible={visible}>
+    <div className="TimerPage-FeelingPopup" data-visible={visible}>
       <div className="ui-container">
-        <div className="TimerPage-ChimePopup-inner" onClick={onStopClick}>
-          <span role="img" aria-label="">🔔</span> Tap to stop ringing
+        <div className="TimerPage-FeelingPopup-inner">
+          <h1 className="TimerPage-FeelingPopup-heading">
+            <span role="img" aria-label="">🔔</span>
+            {' '}
+            How was the sprint?
+          </h1>
+          <div className="TimerPage-FeelingPopup-feelingList">
+            {feelings.map((feeling) => (
+              <FeelingButton
+                feeling={feeling}
+                key={feeling}
+                onClick={onSelect}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
+  );
+};
+
+const FeelingButton: React.FC<{
+  feeling: Activity.ActivityFeeling;
+  onClick: (feeling: Activity.ActivityFeeling) => void;
+}> = ({ feeling, onClick }) => {
+  const emoji = Activity.getEmoji(feeling);
+
+  let text;
+  if (feeling === 'great') {
+    text = 'Great!';
+  } else if (feeling === 'good') {
+    text = 'Good'
+  } else if (feeling === 'bad') {
+    text = 'Bad...'
+  }
+
+  const onButtonClick = () => onClick(feeling);
+
+  return (
+    <button className="TimerPage-FeelingButton" onClick={onButtonClick}>
+      {emoji}
+      <br/>
+      {text}
+    </button>
   );
 };
