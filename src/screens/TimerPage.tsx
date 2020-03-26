83b1a9d30d79764fa67fdde28f855dfa6848cba4
ginpei <ginpei@gmail.com>
2020-03-25 22:57:13 -0700

    organize imports

--- src/screens/TimerPage.tsx
+++ src/screens/TimerPage.tsx
@@ -1,10 +1,10 @@
-import React, { useState, createRef } from 'react';
+import React, { useState } from 'react';
 import { TimerConsole } from '../complexes/TimerConsole';
 import * as Activity from '../models/Activity';
+import { useBeyondSprintEffect } from '../models/Clock';
 import { TimerActivityItem } from '../simples/TimerActivityItem';
 import { TimerForm } from '../simples/TimerForm';
 import './TimerPage.scss';
-import { useBeyondSprintEffect } from '../models/Clock';
 
 export const TimerPage: React.FC = () => {
   const [activityLog] = useState(Activity.dummyActivities);
