370d1392134bb655eebd34905b7938f8cd96156d
ginpei <ginpei@gmail.com>
2020-04-18 11:16:48 -0700

    update to set meta data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import React, { useState } from 'react';
 import { TimerConsole } from '../complexes/TimerConsole';
 import { Activity, ActivityFeeling, dummyActivities } from '../models/Activity';
@@ -88,6 +89,10 @@ const TimerPage: React.FC = () => {
 
   return (
     <div className={styles.TimerPage}>
+      <Head>
+        <title>Pomolife</title>
+        <link rel="shortcut icon" type="image/png" href="/icon-512.png" />
+      </Head>
       <header className={styles.header}>
         <TimerConsole
           currentTask={currentTask}
