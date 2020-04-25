0d930552faa6d8f8b62db7f43b18b99f2220676f
ginpei <ginpei@gmail.com>
2020-04-25 15:09:50 -0700

    comments

--- models/SettingsProvider.tsx
+++ models/SettingsProvider.tsx
@@ -1,6 +1,22 @@
 import React, { useEffect } from 'react';
 import { useSettingsReducer, loadSettings, SettingsContext } from './SettingsService';
 
+/**
+ * @example
+ * // 1. prepare you component
+ * const MyComponent: React.FC = () => {
+ *   // 3. you can use state and reducer
+ *   const [settings, reduceSettings] = useSettingsReducer();
+ *   return <div />;
+ * };
+ *
+ * // 2. wrap your component
+ * const MyComponentProvided = () => (
+ *   <SettingsProvider>
+ *     <MyComponent />
+ *   </SettingsProvider>
+ * );
+ */
 export const SettingsProvider: React.FC = ({ children }) => {
   const [settings, reduceSettings] = useSettingsReducer();
 
